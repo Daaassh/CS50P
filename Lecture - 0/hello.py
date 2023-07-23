93e99063335e7ad79fcff120e114bfacdf39d0ac
BearsDeveloper <120331922+BearDevelopers@users.noreply.github.com>
2023-07-23 17:28:05 -0300

    Trocando a calculator.py
    Commit para trocar o calculator.py é o main.py!

--- Lecture - 0/hello.py
+++ Lecture - 0/hello.py
@@ -1,13 +1,5 @@
 def main():
-    # Pergunta ao usuario o seu nome, Remove os espaços em branco e adiciona as 1 letras das palavras para maisculo!
-    name = input("Qual é o seu nome? ").strip().title()
-    # Diz olá ao mundo!
-    return name
+    name = input("Qual o seu nome: ").strip().title()
+    print(f"Olá, {name}")
 
-# Definindo uma função
-def hello(to="world"):
-    print(f"Olá, {main()}")
-
-
-
-hello()
+main()
