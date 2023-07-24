8af0dad86bf114f74ad321717770051761a9220c
BearsDeveloper <120331922+BearDevelopers@users.noreply.github.com>
2023-07-24 13:40:39 -0300

    Acrecentando comentarios a Lecture - 0;
    Comentarios para tornar o codigo mais detalhado!

--- Lecture - 0/calculator.py
+++ Lecture - 0/calculator.py
@@ -1,7 +1,11 @@
+# Criando a função main para pegar os dados. 
 def main():
     x = int(input("Qual o valor do x: "))
     print(f"O valor do x ao quadrado é de {power(x)}")
 
+# Função para verificar o valor do x ao quadrado!
 def power(x):
     return pow(x, 2) 
+
+# Iniciando a função main
 main()
--- Lecture - 0/hello.py
+++ Lecture - 0/hello.py
@@ -1,5 +1,8 @@
+# Função main para enviar um Olá. Para o usuario!
 def main():
     name = input("Qual o seu nome: ").strip().title()
     print(f"Olá, {name}")
+
+# Iniciando a função main
 
 main()
